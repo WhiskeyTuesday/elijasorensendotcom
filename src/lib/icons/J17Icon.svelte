6626fbcdb29843db083824fe1771a81e27def7b2
Elija Sorensen <3824702+WhiskeyTuesday@users.noreply.github.com>
2026-02-02 11:19:46 -0600

    wow I'm pretty busy for someone unemployed

---
+++ src/lib/icons/J17Icon.svelte
@@ -0,0 +1,14 @@
+<script>
+  let { size = 24, class: className = '' } = $props();
+</script>
+
+<svg
+  width={size}
+  height={size}
+  viewBox="0 0 32 32"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class={className}
+>
+  <polygon points="16,2 28,8 30,20 20,30 8,28 2,14" fill="currentColor"/>
+</svg>
